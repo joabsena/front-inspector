eac462f278bd032a1f9ef51dc52ab1a2ff791e60
Joab Sena <joabarbosa1@hotmail.com>
2024-02-04 08:52:13 -0300

    feat: add validation if param is number

--- src/utils/is-defined.ts
+++ src/utils/is-defined.ts
@@ -1,3 +1,4 @@
-export function isDefined(margin: string) {
-  return !!parseInt(margin, 10);
+export function isDefined(value: string | number) {
+  const numericValue = typeof value === "string" ? parseInt(value, 10) : value;
+  return !isNaN(numericValue);
 }
